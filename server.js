a452c2e842300d7a6fe87240c0fc95bbc5f4baa0
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-11-21 03:10:44 +0100

    feat: add startups data and implement API endpoint for fetching startups

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import { startups } from './data/data';
+
+const celebrity = {
+  type: "action hero",
+  name: "JSON Statham"
+}
 
 const PORT = 8000;
 
 const app = express();
+
+app.get('/api', (request, response) => {
+    response.json(startups);
+});
 
 app.listen(PORT, () => console.log(`server connected on ${PORT}`));
