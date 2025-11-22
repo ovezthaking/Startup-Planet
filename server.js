bb186abff5c24553b2dcde8d1f39f5ecd28335eb
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-11-22 19:59:53 +0100

    Final commit. feat: add cors dependency and middleware for handling cross-origin requests

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 import express, { request, response } from 'express';
 import { apiRouter } from './routes/apiRoutes.js';
+import cors from 'cors';
 
 const PORT = 8000;
 
 const app = express();
 
+app.use(cors)
+
 app.use('/api', apiRouter);
 
 app.use((request, response) => {
-  response.status(404).json({message: "Endpoint not found."});
+  response.status(404).json({message: "Endpoint not found.Please check the API documentation."});
 })
-
+ 
 app.listen(PORT, () => console.log(`server connected on ${PORT}`));
